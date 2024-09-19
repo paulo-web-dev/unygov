d9e81741641b49877aeb94415ea4275531b6c230
paulo-web-dev <paulosergioorfanelli@gmail.com>
2024-09-19 11:59:47 -0300

    Imagem de fundo para configuração de site

---
+++ resources/views/breve.blade.php
@@ -0,0 +1,12 @@
+
+<style>.fullscreen-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    z-index: -1; /* Para garantir que a imagem fique atrás de outros conteúdos */
+}
+</style>
+<img src ="{{url('/fotos-site/unygov.jpeg')}}" class="fullscreen-image">
